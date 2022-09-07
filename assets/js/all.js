0b3a7a7ab3dccfd45a17c50c3e48daf86bcde1aa
Alvin <jimmyjimmy66666@gmail.com>
2022-09-08 00:08:53 +0800

    Update 2022-09-07T16:08:50.939Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,16 @@
 "use strict";
 
-console.log('Hello!');
-$(document).ready(function () {
-  console.log('HesSchool Hello!');
-});
+// console.log('Hello!');
+// $(document).ready(() => {
+//   console.log('HexSchool Hello!');
+// });
+var load = document.querySelector('.loader');
+
+function loader() {
+  setTimeout(function () {
+    load.style.display = 'none';
+  }, 3000);
+}
+
+loader();
 //# sourceMappingURL=all.js.map
