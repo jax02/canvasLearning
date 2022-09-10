7bd20872636e69b60914aa33883b83a24c92b072
Alvin <jimmyjimmy66666@gmail.com>
2022-09-10 13:42:31 +0800

    courseDetail without codemirror

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -10,3 +10,18 @@ function loader (){
     , 3000);
 }
 loader ();
+let swiper = new Swiper(".courseSwiper", {
+  slidesPerView: 'auto',
+  spaceBetween: 20,
+  // slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
